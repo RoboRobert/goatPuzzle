03b022a071ec916278c7c79abff4546b176b102c
RoboRobert <nathan.estep@gmail.com>
2025-11-17 21:38:57 -0500

    finishes combinatoric and adds dimension selector to monte carlo

--- goatpuzzle.py
+++ goatpuzzle.py
@@ -45,8 +45,8 @@ class Combination:
         return (dist1, dist2)
 
 
-def doPuzzleWithClasses():
-    board_dimensions = (3, 8)
+def doCombinatoric():
+    board_dimensions = (5, 5)
     board_area = board_dimensions[0] * board_dimensions[1]
 
     alice_wins = 0
@@ -78,51 +78,63 @@ def doPuzzleWithClasses():
 
             total_combinations += 1
 
-    print(f"STATISTICS FOR {board_dimensions[0]}x{board_dimensions[1]} BOARD:")
+    print(
+        f"\n\nCOMBINATORIC STATISTICS FOR {board_dimensions[0]}x{board_dimensions[1]} BOARD:"
+    )
     print(f"TOTAL COMBINATIONS: {total_combinations}")
 
     print(f"ALICE WINS: {alice_wins}")
     print(f"BOB WINS: {bob_wins}")
     print(f"DRAWS: {draws}")
 
+    increased_odds = (alice_wins / bob_wins) - 1
 
-def doPuzzle():
-    ITERATIONS = 1_000_000
+    print(f"ODDS DIFFERENCE: {increased_odds:.2%}")
+
+
+def doMonteCarlo():
+    ITERATIONS = 1_050_000
+
+    board_dimensions = (5, 5)
 
     alice_wins = 0
     bob_wins = 0
     draws = 0
 
     for iteration in range(0, ITERATIONS):
-        boxes = generateBoxes()
-        winners = simulate(boxes)
+        boxes = generateBoxes(board_dimensions)
+        winners = simulate(boxes, board_dimensions)
         if winners[0] == winners[1]:
             draws += 1
-        else:
-            alice_wins += winners[0]
-            bob_wins += winners[1]
+        alice_wins += winners[0]
+        bob_wins += winners[1]
 
-    print(f"Alice wins: {alice_wins}")
-    print(f"Bob wins: {bob_wins}")
-    print(f"Draws: {draws}")
+    print(
+        f"\n\nMONTE CARLO STATISTICS FOR {board_dimensions[0]}x{board_dimensions[1]} BOARD WITH {ITERATIONS} ITERATIONS:"
+    )
+    print(f"ALICE WINS: {alice_wins}")
+    print(f"BOB WINS: {bob_wins}")
+    print(f"DRAWS: {draws}")
 
     increased_odds = (alice_wins / bob_wins) - 1
 
-    print(f"Increased odds of Alice winning: {increased_odds:.2%}")
+    print(f"ODDS DIFFERENCE: {increased_odds:.2%}")
 
 
-def simulate(boxes: list[list[str]]) -> tuple[int, int]:
+def simulate(boxes: list[list[str]], dimensions: tuple[int, int]) -> tuple[int, int]:
     alice_won = 0
     bob_won = 0
 
-    for position in range(0, 15, 1):
+    board_area = dimensions[0] * dimensions[1]
+
+    for position in range(0, board_area, 1):
         winner: bool = False
 
         # Row-major: (0,0) -> (0,1) -> (0,2) ...
-        alice_pos = (position // 5, position % 5)
+        alice_pos = (position // dimensions[1], position % dimensions[1])
 
         # Column-major: (0,0) -> (1,0) -> (2,0) -> (0, 1)
-        bob_pos = (position % 3, position // 3)
+        bob_pos = (position % dimensions[0], position // dimensions[0])
 
         # Alice wins
         if boxes[alice_pos[0]][alice_pos[1]] == "GOAT":
@@ -141,18 +153,25 @@ def simulate(boxes: list[list[str]]) -> tuple[int, int]:
     return (alice_won, bob_won)
 
 
-def generateBoxes() -> list[list[str]]:
-    boxes: list[list[str]] = [[], [], []]
-    for row in range(0, 3, 1):
-        for col in range(0, 5, 1):
+def generateBoxes(dimensions: tuple[int, int]) -> list[list[str]]:
+    boxes: list[list[str]] = []
+    for row in range(0, dimensions[0]):
+        boxes.append([])
+        for col in range(0, dimensions[1]):
             boxes[row].append("NONE")
 
     # Add goat 1
-    boxes[random.randint(0, 2)][random.randint(0, 4)] = "GOAT"
+    boxes[random.randint(0, dimensions[0] - 1)][
+        random.randint(0, dimensions[1] - 1)
+    ] = "GOAT"
 
     # Add goat 2
     while True:
-        position = (random.randint(0, 2), random.randint(0, 4))
+        position = (
+            random.randint(0, dimensions[0] - 1),
+            random.randint(0, dimensions[1] - 1),
+        )
+
         if boxes[position[0]][position[1]] == "GOAT":
             continue
         else:
@@ -162,6 +181,6 @@ def generateBoxes() -> list[list[str]]:
     return boxes
 
 
-# doPuzzle()
+doMonteCarlo()
 
-doPuzzleWithClasses()
+doCombinatoric()
